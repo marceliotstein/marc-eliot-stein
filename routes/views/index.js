c370cebcaf4ec7f81bd13461883bb754317c3795
marceliotstein <marc.eliot.stein@gmail.com>
2016-02-28 08:18:13 -0500

    added index.js routing for project display on site front page

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,4 +1,5 @@
 var keystone = require('keystone');
+var async = require('async');
 
 exports = module.exports = function(req, res) {
 	
@@ -8,6 +9,29 @@ exports = module.exports = function(req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+	locals.data = {
+		projectlist: [],
+		categories: []
+	};
+	
+	// Load the projectlist
+	view.on('init', function(next) {
+		
+		var q = keystone.list('Project').paginate({
+				page: req.query.page || 1,
+				perPage: 10,
+				maxPages: 10
+			})
+			.where('state', 'published')
+			.sort('-publishedDate')
+			.populate('categories');
+		
+		q.exec(function(err, results) {
+			locals.data.projectlist = results;
+			next(err);
+		});
+		
+	});
 	
 	// Render the view
 	view.render('index');

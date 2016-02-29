d54e34fc335f99079029672bf3b9c64eb57b9ff9
marceliotstein <marc.eliot.stein@gmail.com>
2016-02-29 07:04:26 -0500

    commented out mongoldb remote db in keystone.js

--- keystone.js
+++ keystone.js
@@ -26,7 +26,7 @@ keystone.init({
 	'session': true,
 	'auth': true,
 	'user model': 'User',
-        'mongo': 'mongodb://heroku_02qq5lcx:ti8lr36hu4g858fcir02rio1ll@ds019038.mlab.com:19038/heroku_02qq5lcx'
+        //'mongo': 'mongodb://heroku_02qq5lcx:ti8lr36hu4g858fcir02rio1ll@ds019038.mlab.com:19038/heroku_02qq5lcx'
 
 });
 
